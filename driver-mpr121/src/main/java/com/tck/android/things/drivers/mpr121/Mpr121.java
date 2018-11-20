9cd1e488f78d90c6636dda5babcf3919bb87e577
Wolfgang Gaar <nohum@users.noreply.github.com>
2018-11-20 21:59:15 +0100

    Fixed reference to peripheral service

--- driver-mpr121/src/main/java/com/tck/android/things/drivers/mpr121/Mpr121.java
+++ driver-mpr121/src/main/java/com/tck/android/things/drivers/mpr121/Mpr121.java
@@ -1,7 +1,7 @@
 package com.tck.android.things.drivers.mpr121;
 
 import com.google.android.things.pio.I2cDevice;
-import com.google.android.things.pio.PeripheralManagerService;
+import com.google.android.things.pio.PeripheralManager;
 
 import com.tck.android.things.drivers.mpr121.event.TouchEvent;
 import com.tck.android.things.drivers.mpr121.listener.TouchListener;
@@ -39,7 +39,7 @@ public class Mpr121 implements AutoCloseable {
      * @param bus
      */
     public Mpr121(String bus) throws IOException {
-        PeripheralManagerService pioService = new PeripheralManagerService();
+        PeripheralManager pioService = PeripheralManager.getInstance();
         I2cDevice device = pioService.openI2cDevice(bus, Mpr121Values.MPR121_ADDRESS);
         connect(device);
     }
